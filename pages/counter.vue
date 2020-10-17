e8181d9eb80b0f478ce703cab7b7b6901081d158
andy19910102 <andy19910102@gmail.com>
2020-10-17 17:05:58 +0800

    Project init

---
+++ pages/counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Counter</h1>
+      <!-- TODO: 設計計數器UI -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "counterPage",
+};
+</script>
