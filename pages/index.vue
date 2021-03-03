f94e4496336ddb338bb28c98ea57fe3694445051
andy19910102 <andy19910102@gmail.com>
2021-03-03 18:03:21 +0800

    Update page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="container">
-      <h1>Home Page</h1>
+      <h1>TODO List</h1>
+      <!-- TODO: 設計新增待辦事項UI -->
+      <div class="create-todo-ui"></div>
+      <!-- TODO: 待辦事項列表 -->
+      <div class="todo-list-ui"></div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "indexPage",
+  name: "IndexPage",
 };
 </script>
-
-<style>
-</style>
